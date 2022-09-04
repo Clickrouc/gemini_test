a64d5aaa8a530efaaffd93c7ce337a2e6b903dc5
Nikolay Petrov <clickrouc@gmail.com>
2022-09-04 22:04:52 +0600

    Create basic layout with Ant Design
    Add routing
    Add antd
    Add styled
    Create basic layout with antd tools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import type {FC} from 'react';
 
-function App() {
-	return (
-		<div>
-            Start
-		</div>
-	);
-}
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import styled from 'styled-components';
+
+import Users from './pages/Users';
+import {Layout, Menu, Typography} from 'antd';
+
+const {Content, Footer, Header} = Layout;
+const {Text, Link} = Typography;
+
+// TODO: Need to fix eslint config
+const Styled = {
+	Layout: styled(Layout)`
+		min-height: 100vh;
+	`,
+	Container: styled.div`
+		padding: 24px 50px;
+	`,
+};
+
+const App: FC = () => (
+	<Styled.Layout>
+		<Header>
+			<Menu
+				theme='dark'
+				mode='horizontal'
+				defaultSelectedKeys={['2']}
+				items={[
+					{
+						key: 'users',
+						label: 'Users',
+					},
+				]}
+			/>
+		</Header>
+
+		<Content>
+			<Styled.Container>
+				<Router>
+					<Routes>
+						<Route path='/' element={<Navigate to={'/users'} />} />
+						<Route path='/users' element={<Users />} />
+					</Routes>
+				</Router>
+			</Styled.Container>
+		</Content>
+
+		<Footer>
+			<Text>Created by <Link href='https://github.com/clickrouc' target='_blank'>@clickrouc</Link></Text>
+		</Footer>
+	</Styled.Layout>
+);
 
 export default App;
---
+++ client/src/pages/Users/index.tsx
@@ -0,0 +1,9 @@
+import type {FC} from 'react';
+
+import React from 'react';
+
+const Users: FC = () => (
+	<h1>Users</h1>
+);
+
+export default Users;
